931c59ff9d38a0ea522dd74967eb550e17ac3215
cetecom <cetecom@LC08L1300>
2016-05-02 19:10:21 -0300

    Correcciones menores

--- db_constantes.php
+++ db_constantes.php
@@ -2,8 +2,8 @@
 /*Opciones de conexión a la base de datos*/
 
 define("MYSQL_SERVER","mysql:host=localhost");
-define("MYSQL_USER","root@localhost");
-define("MYSQL_PASS","");
+define("MYSQL_USER","root");
+define("MYSQL_PASS","avaras08");
 define("MYSQL_DB","dbname=dbusuarios");
 
 ?>
--- dbusuarios.sql
+++ dbusuarios.sql
@@ -1,4 +1,4 @@
--
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -6,5 +6,7 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(100) COLLATE utf8_bin NOT NULL,
   `apellido` varchar(100) COLLATE utf8_bin NOT NULL,
   `usuario` varchar(20) COLLATE utf8_bin NOT NULL,
-  `clave` varchar(20) COLLATE utf8_bin NOT NULL
+  `clave` varchar(100) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+insert into usuarios(nombre,usuario,clave) values('Administrador', 'admin','1c7a92ae351d4e21ebdfb897508f59d6');
